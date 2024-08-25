24a8035140504efaee574119341c46154a090dfc
Corayemusic <almiron_ayelen@hotmail.com>
2024-08-25 01:40:04 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || "van-shake.vercel.app",
+  site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon()],
 });
